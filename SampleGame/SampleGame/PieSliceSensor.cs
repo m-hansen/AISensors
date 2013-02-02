de04ff87499b6709ebcb36b17537b72fc46cbd1b
Matthew Hansen <m.hansen@knights.ucf.edu>
2013-02-02 18:08:51 -0500

    TEST

---
+++ SampleGame/SampleGame/PieSliceSensor.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Drawing;
+
+// TODO - return values
+// activation levels
+
+// should an agent be able to trigger two portions at the same time?
+// would it be better to check for outer bounds of sensor by pixel and inner bounds by origin to prevent this?
+
+namespace SampleGame
+{
+    public class PieSliceSensor : Sensor
+    {
+        public int MaxDistance;
+        public float Rotation1;
+        public float Rotation2;
+        public string DisplayText;
+        public int Index;
+        private bool isTriggered;
+        private Vector2 endPoint1;
+        private Vector2 endPoint2;
+        
+        public override void Update(KeyboardState keyboard, List<GameAgent> agentAIList, Vector2 playerPos, float playerRot)
+        {
+            // reinitializing the sensor to not triggered (no agent withing proximity)
+            isTriggered = false;
+
+            // the sensor is active if the key is currently being pushed down
+            Active = keyboard.IsKeyDown(Key);
+
+            // end point of the one side of the pie slice (beginning point is the player position)
+            endPoint1 = CalculateRotatedMovement(new Vector2(0, -1), playerRot + Rotation1) * MaxDistance + playerPos;
+
+            // end point of the other side of the pie slice (beginning point is the player position)
+            endPoint2 = CalculateRotatedMovement(new Vector2(0, -1), playerRot + Rotation2) * MaxDistance + playerPos;
+
+            // pie slice sensors only work for npcs
+            List<GameAgent> npcs = agentAIList.Where(a => a.Type == (int)Enums.AgentType.NPC).ToList();
+
+            foreach (GameAgent agent in npcs)
+            {
+                // if the agent is within the pie slice sensor
+                if (isTriggered = IsInAgentSensorRange(agent, playerPos, endPoint1, endPoint2))
+                    break;
+            }
+        }
+
+        private bool IsInAgentSensorRange(GameAgent agent, Vector2 playerPos, Vector2 endPoint1, Vector2 endPoint2)
+        {
+            Rectangle agentBounds = agent.Bounds;
+
+            // getting the 4 points of the agent
+            Vector2 bottomLeft = new Vector2(agentBounds.Left, agentBounds.Top);
+            Vector2 bottomRight = new Vector2(agentBounds.Left + agentBounds.Width, agentBounds.Top);
+            Vector2 topLeft = new Vector2(agentBounds.Left, agentBounds.Top + agentBounds.Height);
+            Vector2 topRight = new Vector2(bottomRight.X, topLeft.Y);
+
+            return
+                IsPointInTriangle(bottomLeft, playerPos, endPoint1, endPoint2) ||
+                IsPointInTriangle(bottomRight, playerPos, endPoint1, endPoint2) ||
+                IsPointInTriangle(topLeft, playerPos, endPoint1, endPoint2) ||
+                IsPointInTriangle(topRight, playerPos, endPoint1, endPoint2);
+        }
+
+        private bool IsPointInTriangle(Vector2 targetPoint, Vector2 a, Vector2 b, Vector2 c)
+        {
+            // the pie slice sensor creates a triangle, if an agent is within that triangle
+            // then the sensor is triggered. 
+            // NOTE: Since cross products only work for 3D vectors, converting all the vectors to 3D
+            Vector3 _targetPoint = new Vector3(targetPoint, 0);
+            Vector3 _a = new Vector3(a, 0);
+            Vector3 _b = new Vector3(b, 0);
+            Vector3 _c = new Vector3(c, 0);
+
+            return (IsSameSide(_targetPoint, _a, _b, _c) &&
+                IsSameSide(_targetPoint, _b, _a, _c) &&
+                IsSameSide(_targetPoint, _c, _a, _b));
+        }
+
+        private bool IsSameSide(Vector3 point1, Vector3 point2, Vector3 a, Vector3 b)
+        {
+            Vector3 p1 = Vector3.Cross(b - a, point1 - a);
+            Vector3 p2 = Vector3.Cross(b - a, point2 - a);
+
+            return Vector3.Dot(p1, p2) >= 0;
+        }
+
+        public override void Draw(SpriteBatch sprites, Vector2 startPoint, SpriteFont font1)
+        {
+            if (Active)
+            {
+                if (isTriggered)
+                {
+                    DrawingHelper.DrawFastLine(startPoint, endPoint1, Color.Yellow);
+                    DrawingHelper.DrawFastLine(startPoint, endPoint2, Color.Yellow);
+                    DrawingHelper.DrawFastLine(endPoint1, endPoint2, Color.Red);
+
+                    sprites.DrawString(font1, "Pie-Slice Sensor " + DisplayText + ": Triggered", new Vector2(20, 460 + 20 * Index), Color.LightGreen, 0.0f, Vector2.Zero, 0.75f, SpriteEffects.None, 0);
+                }
+                else
+                {
+                    DrawingHelper.DrawFastLine(startPoint, endPoint1, Color.Yellow);
+                    DrawingHelper.DrawFastLine(startPoint, endPoint2, Color.Yellow);
+                }
+            }
+        }
+    }
+}
---
+++ SampleGame/SampleGame/Player.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Drawing;
+
+namespace SampleGame
+{
+    public class Player : GameAgent
+    {
+        public float Speed;  // forward - backward speed
+        private List<Sensor> sensorList = new List<Sensor>();
+
+        public void InitializeSensors()
+        {
+            sensorList.Add(new RangeFinder()
+            {
+                Type = (int)Enums.SensorType.RangeFinder,
+                Rotation = (float)Math.PI / 6,
+                Key = Keys.P,
+                MaxDistance = 150,
+                Index = 0,
+                DirectionText = "Right"
+            });
+
+            sensorList.Add(new RangeFinder()
+            {
+                Type = (int)Enums.SensorType.RangeFinder,
+                Rotation = 0,
+                Key = Keys.P,
+                MaxDistance = 150,
+                Index = 1,
+                DirectionText = "Middle"
+            });
+
+            sensorList.Add(new RangeFinder()
+            {
+                Type = (int)Enums.SensorType.RangeFinder,
+                Rotation = -1 * (float)Math.PI / 6,
+                Key = Keys.P,
+                MaxDistance = 150,
+                Index = 2,
+                DirectionText = "Left"
+            });
+
+            sensorList.Add(new AdjacentAgentSensor()
+            {
+                Type = (int)Enums.SensorType.AgentSensor,
+                Radius = 150,
+                Key = Keys.O,
+                //MaxDistance = 100 // TODO - del
+            });
+
+            sensorList.Add(new PieSliceSensor() // - 60 to 60 degrees
+            {
+                Type = (int)Enums.SensorType.PieSliceSensor,
+                Key = Keys.I,
+                Rotation1 = -1 * (float)Math.PI / 3,
+                Rotation2 = (float)Math.PI / 3,
+                MaxDistance = 150,
+                DisplayText = "(1,0) - Straight Ahead",
+                Index = 0
+            });
+
+            sensorList.Add(new PieSliceSensor() // 60 to 120 degrees
+            {
+                Type = (int)Enums.SensorType.PieSliceSensor,
+                Key = Keys.I,
+                Rotation1 = (float)Math.PI / 3,
+                Rotation2 = 2 * (float)Math.PI / 3,
+                MaxDistance = 150,
+                DisplayText = "(0,1) - Right",
+                Index = 1
+            });
+
+            sensorList.Add(new PieSliceSensor() // 120 to 240 degrees
+            {
+                Type = (int)Enums.SensorType.PieSliceSensor,
+                Key = Keys.I,
+                Rotation1 = 2 * (float)Math.PI / 3,
+                Rotation2 = 4 * (float)Math.PI / 3,
+                MaxDistance = 150,
+                DisplayText = "(-1,0) - Backwards",
+                Index = 2
+            });
+
+            sensorList.Add(new PieSliceSensor() // 240 to 300 degrees
+            {
+                Type = (int)Enums.SensorType.PieSliceSensor,
+                Key = Keys.I,
+                Rotation1 = 4 * (float)Math.PI / 3,
+                Rotation2 = 5 * (float)Math.PI / 3,
+                MaxDistance = 150,
+                DisplayText = "(0,-1) - Left",
+                Index = 3
+            });
+
+
+            //TODO - delete
+            //sensorList.Add(new Sensor() // - 60 to 60 degrees
+            //{
+            //    Type = (int)Enums.SensorType.PieSliceSensor,
+            //    Rotation1 = -1 * (float)Math.PI / 3,
+            //    Rotation2 = (float)Math.PI / 3,
+            //    //MaxDistance = 100
+            //});
+
+            //sensorList.Add(new Sensor() // 60 to 120 degrees
+            //{
+            //    Type = (int)Enums.SensorType.PieSliceSensor,
+            //    Rotation1 = (float)Math.PI / 3,
+            //    Rotation2 = 2 * (float)Math.PI / 3,
+            //    //MaxDistance = 100
+            //});
+
+            //sensorList.Add(new Sensor() // 120 to 240 degrees
+            //{
+            //    Type = (int)Enums.SensorType.PieSliceSensor,
+            //    Rotation1 = 2 * (float)Math.PI / 3,
+            //    Rotation2 = 4 * (float)Math.PI / 3,
+            //    //MaxDistance = 100
+            //});
+
+            //sensorList.Add(new Sensor() // 240 to 300 degrees
+            //{
+            //    Type = (int)Enums.SensorType.PieSliceSensor,
+            //    Rotation1 = 4 * (float)Math.PI / 3,
+            //    Rotation2 = 5 * (float)Math.PI / 3,
+            //    //MaxDistance = 100
+            //});
+        }
+
+        public Vector2 CalculateRotatedMovement(Vector2 point, float rotation)
+        {
+            return Vector2.Transform(point, Matrix.CreateRotationZ(rotation));
+        }
+
+        public void Update(GameTime gameTime, KeyboardState keyboardStateCurrent, KeyboardState keyboardStatePrevious, 
+            MouseState mouseStateCurrent, MouseState mouseStatePrevious, List<GameAgent> agentAIList,
+            int windowWidth, int windowHeight)
+        {
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // rotation
+            if (keyboardStateCurrent.IsKeyDown(Keys.Left) || keyboardStateCurrent.IsKeyDown(Keys.A))
+                Rotation -= (elapsedTime * RotationSpeed) % MathHelper.TwoPi;
+            if (keyboardStateCurrent.IsKeyDown(Keys.Right) || keyboardStateCurrent.IsKeyDown(Keys.D))
+                Rotation += (elapsedTime * RotationSpeed) % MathHelper.TwoPi;
+
+            // movement
+            if (keyboardStateCurrent.IsKeyDown(Keys.Up) || keyboardStateCurrent.IsKeyDown(Keys.W))
+            {
+                Vector2 nextPos = CalculateRotatedMovement(new Vector2(0, -1), Rotation) * Speed + Position;
+
+                if (IsValidMove(nextPos, agentAIList, windowWidth, windowHeight))
+                    Position = nextPos;
+            }
+            if (keyboardStateCurrent.IsKeyDown(Keys.Down) || keyboardStateCurrent.IsKeyDown(Keys.S))
+            {
+                Vector2 nextPos = CalculateRotatedMovement(new Vector2(0, 1), Rotation) * Speed + Position;
+
+                if (IsValidMove(nextPos, agentAIList, windowWidth, windowHeight))
+                    Position = nextPos;
+            }
+
+            foreach (Sensor sensor in sensorList)
+            {
+                sensor.Update(keyboardStateCurrent, agentAIList, this.Position, this.Rotation);
+            }
+
+            base.Update(gameTime);
+        }
+
+        private bool IsValidMove(Vector2 nextPos, List<GameAgent> agentAIList, int windowWidth, int windowHeight)
+        {
+            Rectangle rect = new Rectangle
+            (
+                (int)(nextPos.X - Origin.X * Scale),
+                (int)(nextPos.Y - Origin.Y * Scale),
+                FrameWidth,
+                FrameHeight
+            );
+
+            bool collision = false;
+
+            foreach (GameAgent agent in agentAIList)
+            {
+                if (collision = agent.Bounds.Intersects(rect))
+                    break;
+            }
+
+            return (!collision && rect.Left > 0 && rect.Left + rect.Width < windowWidth && rect.Top > 0 && rect.Top + rect.Height < windowHeight);
+        }
+
+        public override void Draw(SpriteBatch sprites, SpriteFont font1)
+        {
+            //DrawingHelper.Begin(PrimitiveType.LineList);
+
+            foreach (Sensor sensor in sensorList)
+            {
+                sensor.Draw(sprites, this.Position, font1);
+            }
+
+            //DrawingHelper.End();
+
+            base.Draw(sprites, font1);
+        }
+    }
+}
